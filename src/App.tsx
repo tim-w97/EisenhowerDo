052244f862a99ec109b539450b172bef00e0481a
Tim <timwagner997@gmail.com>
2024-04-13 20:55:45 +0200

    created a navigation stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import MyTodosScreen from './screens/MyTodosScreen.tsx';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AddTodoScreen from './screens/AddTodoScreen.tsx';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
-  return <MyTodosScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MyTodos">
+        <Stack.Screen name="MyTodos" component={MyTodosScreen} />
+        <Stack.Screen name="AddTodo" component={AddTodoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
