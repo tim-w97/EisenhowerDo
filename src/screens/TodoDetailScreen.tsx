2f6fcac18cf2ac3ccede8ec40e9980b2af0d7d96
Tim <timwagner997@gmail.com>
2024-04-30 10:25:44 +0200

    added boilerplate for todo detail screen (master detail view)

---
+++ src/screens/TodoDetailScreen.tsx
@@ -0,0 +1,24 @@
+import {Button, SafeAreaView, Text, View} from 'react-native';
+import globalStyles from '../styles/globalStyles.ts';
+import React from 'react';
+
+function TodoDetailScreen() {
+  function onMarkTodoAsCompleted() {}
+
+  return (
+    <SafeAreaView style={globalStyles.safeArea}>
+      <View>
+        <Text>Hier kommt der Titel hin</Text>
+        <Text>Hier kommt der Text hin hin</Text>
+      </View>
+      <View>
+        <Button
+          title="als erledigt markieren"
+          onPress={onMarkTodoAsCompleted}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default TodoDetailScreen;
