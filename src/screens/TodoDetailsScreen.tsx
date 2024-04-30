b407f0112b51d7f8516ac48d7e56afaa88bd589e
Tim <timwagner997@gmail.com>
2024-04-30 10:54:20 +0200

    added types for navigation and route

--- src/screens/TodoDetailsScreen.tsx
+++ src/screens/TodoDetailsScreen.tsx
@@ -1,14 +1,28 @@
 import {Button, SafeAreaView, Text, View} from 'react-native';
 import globalStyles from '../styles/globalStyles.ts';
 import React from 'react';
+import {NavigationProp, RouteProp} from '@react-navigation/native';
 
-function TodoDetailsScreen() {
-  function onMarkTodoAsCompleted() {}
+type StackParamList = {
+  TodoDetails: {todoID: string};
+};
+
+type Props = {
+  navigation: NavigationProp<StackParamList, 'TodoDetails'>;
+  route: RouteProp<StackParamList, 'TodoDetails'>;
+};
+
+function TodoDetailsScreen({route, navigation}: Props) {
+  const {todoID} = route.params;
+
+  function onMarkTodoAsCompleted() {
+    navigation.goBack();
+  }
 
   return (
     <SafeAreaView style={globalStyles.safeArea}>
       <View>
-        <Text>Hier kommt der Titel hin</Text>
+        <Text>{todoID}</Text>
         <Text>Hier kommt der Text hin hin</Text>
       </View>
       <View>
