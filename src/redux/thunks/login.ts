014e43a3392c3d39161ee008c271622851474132
Tim <timwagner997@gmail.com>
2024-05-07 09:46:20 +0200

    created a thunk for the user login

---
+++ src/redux/thunks/login.ts
@@ -0,0 +1,23 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import Config from 'react-native-config';
+
+export const fetchTodos = createAsyncThunk(
+  // This argument is the action name
+  'user/login',
+
+  // This function contains async logic of a side effect
+  async (_, thunkAPI) => {
+    const url = `${Config.API_URL}/login`;
+
+    // TODO: need auth first
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+      return thunkAPI.rejectWithValue(response.statusText);
+    }
+
+    const {token} = await response.json();
+
+    return token;
+  },
+);
