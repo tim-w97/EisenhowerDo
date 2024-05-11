86185493160e33a1c23d34e4b4982f92f85c9f1c
Tim <timwagner997@gmail.com>
2024-05-11 19:50:42 +0200

    added thunk to share a todo

---
+++ src/redux/thunks/shareTodo.ts
@@ -0,0 +1,21 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import Config from 'react-native-config';
+import {RootState} from '../types/rootState.ts';
+
+export const addTodo = createAsyncThunk(
+  // This argument is the action name
+  'todos/share',
+
+  // This function contains async logic of a side effect
+  async (_, thunkAPI) => {
+    const url = `${Config.API_URL}/todos`;
+
+    const {user} = thunkAPI.getState() as RootState;
+
+    const headers = {
+      Authorization: `Bearer ${user.token}`,
+    };
+
+    // TODO: implement me
+  },
+);
