3f6ba8fb81593a0c838bc9473031dc701db49151
Tim <timwagner997@gmail.com>
2024-05-11 23:06:36 +0200

    don't need these reducers anymore

--- src/redux/reducers/addTodo.ts
+++
@@ -1,9 +0,0 @@
-import {PayloadAction} from '@reduxjs/toolkit';
-import Todo from '../../types/todo.ts';
-import TodosState from '../../types/todosState.ts';
-
-function addTodo(state: TodosState, action: PayloadAction<Todo>) {
-  state.todos = [...state.todos, action.payload];
-}
-
-export default addTodo;
