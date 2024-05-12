964b95fb663b2311fcf0a3b99a58b7c48e2e47cc
Tim <timwagner997@gmail.com>
2024-05-12 11:56:48 +0200

    changed design of todo details screen

--- src/screens/TodoDetailsScreen.tsx
+++ src/screens/TodoDetailsScreen.tsx
@@ -1,4 +1,4 @@
-import {Button, SafeAreaView, Text, View} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text, TextInput} from 'react-native';
 import globalStyles from '../styles/globalStyles.ts';
 import React from 'react';
 import {NavigationProp, RouteProp} from '@react-navigation/native';
@@ -17,28 +17,70 @@ type Props = {
 function TodoDetailsScreen({route, navigation}: Props) {
   const todo = useSelector(selectSingleTodo(route.params.todoID));
 
+  let currentTitle: string = '';
+  let currentText: string = '';
+
   if (!todo) {
     throw new Error("Todo with id ${route.params.todoID} doesn't exist.");
   }
 
-  function onMarkTodoAsCompleted() {
+  function onComplete() {
+    // TODO: Send a DELETE request to the server
+
     navigation.goBack();
   }
 
   return (
     <SafeAreaView style={globalStyles.safeArea}>
-      <View>
-        <Text>{todo.title}</Text>
-        <Text>{todo.text}</Text>
-      </View>
-      <View>
-        <Button
-          title="als erledigt markieren"
-          onPress={onMarkTodoAsCompleted}
-        />
-      </View>
+      <Text style={[globalStyles.bigTitle, styles.smallBottomMargin]}>
+        Titel
+      </Text>
+
+      <TextInput
+        value={todo.title}
+        editable={false}
+        style={[
+          globalStyles.textInput,
+          styles.textInput,
+          styles.bigBottomMargin,
+        ]}
+        placeholder="Titel"
+        onChangeText={text => (currentTitle = text)}
+      />
+      <Text style={[globalStyles.bigTitle, styles.smallBottomMargin]}>
+        Beschreibung
+      </Text>
+
+      <TextInput
+        value={todo.text}
+        editable={false}
+        style={[
+          globalStyles.textInput,
+          styles.textInput,
+          styles.bigBottomMargin,
+        ]}
+        multiline={true}
+        textAlignVertical="top"
+        numberOfLines={5}
+        placeholder="Beschreibung"
+        onChangeText={text => (currentText = text)}
+      />
+      <Button title="Fertig" onPress={onComplete} />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    borderColor: 'lightgray',
+    color: 'black',
+  },
+  smallBottomMargin: {
+    marginBottom: 20,
+  },
+  bigBottomMargin: {
+    marginBottom: 50,
+  },
+});
 
 export default TodoDetailsScreen;
