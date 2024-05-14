7992e8204125656e5b53d5e99e1f37afbf5c7f3e
Tim <timwagner997@gmail.com>
2024-05-14 08:36:54 +0200

    added check icon to the checkbox view

--- src/views/Checkbox.tsx
+++ src/views/Checkbox.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import globalStyles from '../styles/globalStyles.ts';
+import Icon from 'react-native-vector-icons/Entypo';
 
 type CheckboxProps = {
   style?: any;
@@ -12,12 +13,21 @@ export default function Checkbox({style, title, onToggle}: CheckboxProps) {
   return (
     <View style={[styles.container, style]} onTouchEnd={onToggle}>
       <Text style={globalStyles.bigTitle}>{title}</Text>
-      <View style={styles.checkbox} />
+
+      <View style={styles.stackedContainer}>
+        <View style={styles.checkbox} />
+        <Icon style={styles.checkIcon} name="check" size={50} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  stackedContainer: {
+    width: 50,
+    height: 50,
+    position: 'relative',
+  },
   container: {
     display: 'flex',
     flexDirection: 'row',
@@ -26,10 +36,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   checkbox: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+
     width: 50,
     height: 50,
     borderWidth: 5,
     borderRadius: 5,
     borderColor: 'lightblue',
+  },
+  checkIcon: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
   },
 });
