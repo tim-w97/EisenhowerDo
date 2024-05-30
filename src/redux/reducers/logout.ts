901f82c855412c55c8b51bfde0b579f278c1d7ae
Tim <timwagner997@gmail.com>
2024-05-30 16:09:38 +0200

    re-add default credentials after logout

--- src/redux/reducers/logout.ts
+++ src/redux/reducers/logout.ts
@@ -1,6 +1,9 @@
 import {UserState} from '../../types/userState.ts';
 
 export default function logout(state: UserState) {
+  state.credentials.username = 'lisa';
+  state.credentials.password = 'check';
+
   state.token = null;
   state.error = null;
   state.status = 'idle';
