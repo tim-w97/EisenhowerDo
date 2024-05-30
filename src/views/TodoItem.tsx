ce3e92a72993392daab6cab959d501aca934dd4c
Tim <timwagner997@gmail.com>
2024-05-30 17:24:09 +0200

    added priority colors to todos

--- src/views/TodoItem.tsx
+++ src/views/TodoItem.tsx
@@ -10,11 +10,32 @@ type TodoItemProps = {
 export default function TodoItem({todo, onTap}: TodoItemProps) {
   return (
     <View style={styles.container}>
-      <View style={styles.todoItem} onTouchEnd={() => onTap(todo.id)}>
+      <View
+        style={[styles.todoItem, getPriorityColor()]}
+        onTouchEnd={() => onTap(todo.id)}>
         <Text>{todo.title}</Text>
       </View>
     </View>
   );
+
+  function getPriorityColor() {
+    let backgroundColor = 'lightgray';
+
+    if (todo.isImportant && todo.isUrgent) {
+      // TODO: create a custom scheme for the whole app
+      backgroundColor = '#F5C2C1';
+    }
+
+    if (todo.isImportant && !todo.isUrgent) {
+      backgroundColor = 'lightgreen';
+    }
+
+    if (!todo.isImportant && todo.isUrgent) {
+      backgroundColor = 'lightyellow';
+    }
+
+    return {backgroundColor};
+  }
 }
 
 const styles = StyleSheet.create({
@@ -24,7 +45,6 @@ const styles = StyleSheet.create({
   },
 
   todoItem: {
-    backgroundColor: 'lightyellow',
     padding: 10,
     aspectRatio: 1,
   },
