275f82eddd22eb3738995135a5e075454870b4bb
Tim <timwagner997@gmail.com>
2024-06-02 01:02:13 +0200

    added input field for other username

--- src/screens/ShareTodoScreen.tsx
+++ src/screens/ShareTodoScreen.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import globalStyles from '../styles/globalStyles.ts';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Animated} from 'react-native';
+import {Animated, StyleSheet, TextInput} from 'react-native';
 import Text = Animated.Text;
 import FixedBottomButton from '../views/FixedBottomButton.tsx';
 import {NavigationProp} from '@react-navigation/native';
 import {RootStackParamList} from '../types/rootStackParamList.ts';
 import Snackbar from 'react-native-snackbar';
+import todosSlice from '../redux/slices/todosSlice.ts';
+import {useAppDispatch} from '../redux/hooks/useAppDispatch.ts';
+import selectUserToShareTodoWith from '../redux/selectors/selectUserToShareTodoWith.ts';
+import useAppSelector from '../redux/hooks/useAppSelector.ts';
 
 type Props = {
   navigation: NavigationProp<RootStackParamList>;
 };
 
 export default function ShareTodoScreen({navigation}: Props) {
+  const dispatch = useAppDispatch();
+  const username = useAppSelector(selectUserToShareTodoWith());
+
   function onShare() {
+    dispatch(todosSlice.actions.clearUserToShareTodoWith());
+
     // Go back to MyTodos
     navigation.goBack();
     navigation.goBack();
@@ -25,7 +34,18 @@ export default function ShareTodoScreen({navigation}: Props) {
 
   return (
     <SafeAreaView style={globalStyles.safeArea}>
-      <Text>Jetzt teilen</Text>
+      <Text style={[globalStyles.bigTitle, styles.smallBottomMargin]}>
+        Name des anderen Benutzers
+      </Text>
+
+      <TextInput
+        value={username}
+        style={[globalStyles.textInput, styles.bigBottomMargin]}
+        placeholder="Benutzername"
+        onChangeText={user =>
+          dispatch(todosSlice.actions.setUserToShareTodoWith(user))
+        }
+      />
       <FixedBottomButton
         text="Jetzt teilen"
         onTap={onShare}
@@ -34,3 +54,12 @@ export default function ShareTodoScreen({navigation}: Props) {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  smallBottomMargin: {
+    marginBottom: 20,
+  },
+  bigBottomMargin: {
+    marginBottom: 50,
+  },
+});
