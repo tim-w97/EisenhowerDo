560ef6b5e1509c89098a3260324265240833834e
Tim <timwagner997@gmail.com>
2024-06-03 22:46:53 +0200

    added some more comments for explanation

--- src/types/rootStackParamList.ts
+++ src/types/rootStackParamList.ts
@@ -1,13 +1,17 @@
-// define parameters for each route
+// Define parameters for each route
 export type RootStackParamList = {
+  // Onboarding
   Onboarding1: undefined;
   Onboarding2: undefined;
   Onboarding3: undefined;
 
+  // Todo-related screens
   AddTodo: undefined;
   EditTodo: undefined;
-  Login: undefined;
   MyTodos: undefined;
   TodoDetails: undefined;
   ShareTodo: undefined;
+
+  // Authentication
+  Login: undefined;
 };
