43ce1598c40a3ca45992bf719b9ef27e0d4125f3
Tim <timwagner997@gmail.com>
2024-06-04 15:49:19 +0200

    added content to first onboarding screen

--- src/screens/onboarding/OnboardingScreen1.tsx
+++ src/screens/onboarding/OnboardingScreen1.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import globalStyles from '../../styles/globalStyles.ts';
-import {Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import FixedBottomButton from '../../views/FixedBottomButton.tsx';
 import {useNavigation} from '@react-navigation/native';
+import text from '../../styles/text.ts';
 
 export default function OnboardingScreen1() {
   const navigation = useNavigation();
@@ -14,8 +15,21 @@ export default function OnboardingScreen1() {
 
   return (
     <SafeAreaView style={globalStyles.safeArea}>
-      <Text>Onboarding Teil 1</Text>
+      <Text style={[styles.bigBottomMargin, globalStyles.bigTitle]}>Hi!</Text>
+      <Text style={text.defaultText}>
+        Schön, dass du da bist! Willkommen bei EisenhowerDo, deiner neuen
+        Todo-App. Ich freue mich, dass du dich für meine App entschieden hast,
+        um deine Aufgaben zu organisieren und deine Produktivität zu steigern.
+        Lass uns gemeinsam deine Ziele erreichen und deinen Alltag effizienter
+        gestalten.
+      </Text>
       <FixedBottomButton text="Weiter" onTap={onContinue} />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  bigBottomMargin: {
+    marginBottom: 50,
+  },
+});
