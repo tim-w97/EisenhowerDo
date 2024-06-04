e9637a87d6f9b58db3fa7c7bb9fad4f98c2bfa18
Tim <timwagner997@gmail.com>
2024-06-04 22:31:45 +0200

    added explanation comment

--- src/types/rootStackParamList.ts
+++ src/types/rootStackParamList.ts
@@ -1,4 +1,7 @@
 // Define parameters for each route
+
+// My routes don't have any parameters because they communicate
+// via the redux store to each other
 export type RootStackParamList = {
   // Onboarding
   Onboarding1: undefined;
